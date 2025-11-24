3544109d3eb9a9a62afc121cc7956079b0c29f3f
Bartek Parapura <b.parapura@student.uw.edu.pl>
2025-11-24 09:09:18 +0100

    Added implementation of the projection method.

--- dens.py
+++ dens.py
@@ -2,13 +2,46 @@ import numpy as np
 from testing import getXYZtoUVWT, getTestData, getBraggForTrack
 import matplotlib.pyplot as plt
 
+from itertools import product
+
 def vLength(v :np.ndarray):
     return np.sqrt(np.sum(np.square(v)))
+
+def vPerp(v:np.ndarray) -> np.ndarray:
+    return np.array([-v[1], v[0]])
+
+# gets projections of the points that lie closer than epsilon to the line
+def getProjections(points, vals, p0, d, epsilon):
+    v = points - p0
+
+    proj = np.dot(v, d)[:, None] * d
+
+    return proj[np.linalg.norm(v - proj) < epsilon], vals[np.linalg.norm(v - proj) < epsilon]
+
+# gets the distribution on a line from a single image through the projection method
+def getDistributionProjections(points, epsilon, histogram):
+    direction = points[-1,:] - points[0,:]
+    
+    direction = direction / vLength(direction)
+        
+    histogram_coord = np.array([np.array(tup) for tup in product(list(range(histogram.shape[0])), list(range(histogram.shape[1]))) if histogram[*tup] != 0])
+    
+    vals = histogram[histogram_coord[:,0], histogram_coord[:,1]]
+    
+    direction_perp = vPerp(direction)
+    
+    distribution = []
+    
+    for point in points():
+        histogram_coord_p, vals_p = getProjections(histogram_coord, vals, point, direction_perp, epsilon)
+        distribution += [np.average(vals_p, weights = 1 / (np.linalg.norm(histogram_coord_p - point) + 1))]
+    
+    return np.array(distribution)
 
 def getDensity(points, histograms):
     result = []
     for point in points.astype(np.int32):
-        result += [np.array([histograms[i][point[3]][point[i]] for i in range(3)])]
+        result += [np.array([histograms[point[3]][point[i]][i] for i in range(3)])]
     return np.array([np.sum(res) / 3 for res in result])
 
 def estimateMiddle(images, tanget, vertex, start, end):
@@ -22,18 +55,21 @@ def estimateMiddle(images, tanget, vertex, start, end):
         point += direction_vector
     measuring_points = np.array(measuring_points)
     measuring_points_uvwt = getXYZtoUVWT(measuring_points)
-    density = getDensity(measuring_points_uvwt, images)
+    
+    distribution = np.average(
+        np.array([getDistributionProjections(measuring_points_uvwt[[i,3],:], 0.5, images[i]) for i in range(3)]), 
+        axis=0)
     
     last_best_point = None
     last_best_score = np.inf
     for point in measuring_points:
         bragg = getBraggForTrack(np.array([start, point, end]), (end + start) / 2)[2]
         bragg = bragg[bragg[:,0] <= 75.0]
-        score = vLength(density - bragg[:,1])
+        score = vLength(distribution - bragg[:,1])
         
         if score < last_best_score:
             last_best_point = point
             last_best_score = score
-        
+    
     return last_best_point
 estimateMiddle(*getTestData('middle'))

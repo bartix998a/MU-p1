62214845f213fe159f124f4445088055a9ae8a82
Bartek Parapura <b.parapura@student.uw.edu.pl>
2025-12-07 22:27:16 +0100

    Fixes

--- solution.py
+++ solution.py
@@ -7,7 +7,7 @@ from testing import getTestData
 
 def solution(images :list[np.ndarray], denoising :Literal["threshold", "gaussian", "median", "morphological", "wavelet", "fft"]):
     images_denoised = np.array([denoise(image, denoising) for image in images])
-    points = reconstruct_from_histograms_notebook((images_denoised, None,None, None, None))
+    points = reconstruct_from_histograms_notebook((images_denoised, None, None, None, None))
     start, end = points["ep0_mm"], points["ep1_mm"]
     middle = estimateMiddle(images_denoised, start, end)
     return start, end, middle
@@ -20,11 +20,13 @@ def estimateAccuracy(n_calls = 100, where :Literal['noise', 'fit', 'edges', 'mid
     for i in range(n_calls):
         if where == 'noise':
             histograms, clear_histograms = getTestData('noise') #type: ignore
+            histograms = np.array([[denoise(image, 'gaussian') for image in histograms]])
             results += [np.linalg.matrix_norm(hist[0] - hist[1]) for hist in zip(histograms, clear_histograms)]
         elif where == 'fit':
             hist, start_gt, end_gt = getTestData('fit') #type: ignore
-            points = reconstruct_from_histograms_notebook(hist)
+            points = reconstruct_from_histograms_notebook((hist, None, None, None, None))
             start, end = points["ep0_mm"], points["ep1_mm"]
+            print(np.linalg.norm(end -  start))
             
             results += [np.linalg.norm(start - start_gt), np.linalg.norm(end - end_gt)]
         elif where == 'middle':
@@ -41,4 +43,4 @@ def estimateAccuracy(n_calls = 100, where :Literal['noise', 'fit', 'edges', 'mid
     
     return np.average(np.array(results))
 
-estimateAccuracy()
+print(estimateAccuracy(where='noise'))
